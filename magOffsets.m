37b6eb6b03de043c77a23c1186f84086b8ed8307
ebermac <eber_macmar@hotmail.com>
2021-05-21 17:01:35 -0500

    Working angles, position needs improvement

---
+++ magOffsets.m
@@ -0,0 +1,3 @@
+x_offset = (max(out.mag.signals.values(:,1)) + min(out.mag.signals.values(:,1)))/2
+y_offset = (max(out.mag.signals.values(:,2)) + min(out.mag.signals.values(:,2)))/2
+z_offset = (max(out.mag.signals.values(:,3)) + min(out.mag.signals.values(:,3)))/2
